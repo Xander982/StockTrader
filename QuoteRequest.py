043a66daff0ff52f4dc65ee7106f8848f53dd62e
Xander982 <51178927+Xander982@users.noreply.github.com>
2020-07-15 20:35:51 -0400

    Update QuoteRequest.py

--- QuoteRequest.py
+++ QuoteRequest.py
@@ -5,3 +5,21 @@
 # Store ticker data in individual CSV per ticker name
 # Create new data CSV if one does not exist for the ticker name
 # Store date, time, price, high, low, volume, open, close
+"Previous Close": "366.53",
+    "Open": "365.00",
+    "Bid": "358.95 x 1200",
+    "Ask": "359.00 x 800",
+    "Day's Range": "358.52 - 368.77",
+    "52 Week Range": "192.58 - 372.38",
+    "Volume": "48,155,849",
+    "Avg. Volume": "38,750,285",
+    "Market Cap": "1.561T",
+    "Beta (5Y Monthly)": "1.17",
+    "PE Ratio (TTM)": "28.29",
+    "EPS (TTM)": 12.728,
+    "Earnings Date": "2020-07-28 to 2020-08-03",
+    "Forward Dividend & Yield": "3.28 (0.89%)",
+    "Ex-Dividend Date": "May 08, 2020",
+    "1y Target Est": 332.43,
+    "ticker": "aapl",
+    "url": "http://finance.yahoo.com/quote/aapl?p=aapl"
